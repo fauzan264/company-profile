73ac596eba2c06264e674ded2a886e65beb1864c
Fauzan <fauzan.c4@gmail.com>
2025-07-06 16:37:09 +0700

    create backendless lib

---
+++ src/lib/backendless.ts
@@ -0,0 +1,8 @@
+import Backendless from "backendless";
+
+const APP_ID = '1D18B8C1-4703-423E-84F8-771A9CEA5304';
+const API_KEY = '06EC1D0D-6CE2-496A-AD00-352B34406911';
+
+Backendless.initApp(APP_ID, API_KEY);
+
+export default Backendless
