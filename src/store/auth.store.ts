d10e385cb8834e8ae1b3125486f363a252b86fbe
Fauzan <fauzan.c4@gmail.com>
2025-07-06 16:40:17 +0700

    feat: add auth.store.ts

---
+++ src/store/auth.store.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+export type TAuth = {
+  username: string;
+  email: string;
+  userId: string;
+};
+
+export interface IAuthStoreStateProps {
+  auth: TAuth;
+  setAuth: ({ username, email, userId }: TAuth) => void;
+}
+
+export const authStore = create<IAuthStoreStateProps>()(
+  persist(
+    (set) => ({
+      auth: {
+        username: "",
+        email: "",
+        userId: "",
+      },
+      setAuth: ({ username, email, userId }: TAuth) =>
+        set(() => ({
+          auth: { username: username, email: email, userId: userId },
+        })),
+    }),
+    {
+      name: "auth-session",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ auth: { userId: state?.auth?.userId } }),
+    }
+  )
+);
