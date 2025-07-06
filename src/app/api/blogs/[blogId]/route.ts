c1ff785b3710930b9c25cfe6746548147bb752b9
Fauzan <fauzan.c4@gmail.com>
2025-07-06 16:50:03 +0700

    fix: API blog by id

--- src/app/api/blogs/[blogId]/route.ts
+++ src/app/api/blogs/[blogId]/route.ts
@@ -1,61 +1,34 @@
-import Backendless from "backendless";
+import Backendless from "@/lib/backendless";
 import { NextRequest, NextResponse } from "next/server";
 import { TABLE_NAME_BLOGS } from "../route";
-import { IBlog } from "../route";
+import { IBlog } from "@/features/blog/types";
 import slugify from "slugify";
 
-export async function GET(_: NextRequest, { params }: { params: { blogId: string } }) {
-  try {
-    const { blogId } = params
-
-    const response = await Backendless.Data.of(TABLE_NAME_BLOGS).findById(blogId) as IBlog
-
-    const responseData =  {
-      id: response.objectId,
-      title: response.title,
-      slug: response.slug,
-      description: response.description,
-      image: response.image,
-      created_by: response.created_by,
-      created_at: response.created
-    }
-
-    return NextResponse.json(
-      {
-        message: `Get product detail successfull`,
-        data: responseData
-      },
-      { status: 200 }
-    )
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json(
-      {
-        message: "Something went error"
-      },
-      { status: 500 }
-    )
-  }
-}
-
-export async function PUT(req: NextRequest, { params }: { params: { blogId: string } }) {
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { blogId: string } }
+) {
   try {
     const { blogId } = await params;
-    
-    const {
-      title,
-      image,
-      description
-    } = await req.json()
-  
-    const response = await Backendless.Data.of(TABLE_NAME_BLOGS).save({
-      objectId: blogId,
-      title: title,
-      slug: slugify(title, { lower: true }),
-      image: image,
-      description: description
-    }) as IBlog
-    
+
+    const queryBuilder = Backendless.DataQueryBuilder.create().setWhereClause(
+      `slug = '${blogId}'`
+    );
+    const response = (await Backendless.Data.of(TABLE_NAME_BLOGS).findFirst(
+      queryBuilder
+    )) as IBlog;
+
+    console.log(">>>", response);
+    if (response === undefined) {
+      return NextResponse.json(
+        {
+          status: "fail",
+          message: "Failed Get Data Blog, Data Not Found",
+        },
+        { status: 404 }
+      );
+    }
+
     const responseData = {
       id: response.objectId,
       title: response.title,
@@ -63,23 +36,71 @@ export async function PUT(req: NextRequest, { params }: { params: { blogId: stri
       description: response.description,
       image: response.image,
       created_by: response.created_by,
-      created_at: response.created
-    }
-  
+      created_at: response.created,
+    };
+
     return NextResponse.json(
       {
-        message: `Update blog successful`,
-        data: responseData
+        message: `Get product detail successfull`,
+        data: responseData,
       },
       { status: 200 }
-    )
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return NextResponse.json(
       {
-        message: 'Something went error'
+        status: "error",
+        message: `${error}`,
       },
       { status: 500 }
-    )
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { blogId: string } }
+) {
+  try {
+    const { blogId } = await params;
+
+    const { title, image, description } = await req.json();
+
+    const response = (await Backendless.Data.of(TABLE_NAME_BLOGS).save({
+      objectId: blogId,
+      title: title,
+      slug: slugify(title, { lower: true }),
+      image: image,
+      description: description,
+    })) as IBlog;
+
+    const responseData = {
+      id: response.objectId,
+      title: response.title,
+      slug: response.slug,
+      description: response.description,
+      image: response.image,
+      created_by: response.created_by,
+      created_at: response.created,
+    };
+
+    return NextResponse.json(
+      {
+        status: "success",
+        message: "Blog updated successfully",
+        data: responseData,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: "error",
+        message: "Failed to update blog. Please try again later.",
+      },
+      { status: 500 }
+    );
   }
 }
